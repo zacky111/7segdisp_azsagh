e1f629d462ed4f5a9c6c8972122f32f7068e811c
Jakub Ciura <jakub.ciura@gmail.com>
2025-08-08 09:47:02 +0200

    start of communication

---
+++ test_komunikacja.py
@@ -0,0 +1,23 @@
+import serial
+
+# Otwórz port szeregowy
+ser = serial.Serial(
+port='COM3',  
+baudrate=1200,        
+bytesize=serial.EIGHTBITS,  
+parity=serial.PARITY_NONE,  
+stopbits=serial.STOPBITS_ONE,  
+timeout=1  
+)
+
+print(f"Otwarty port: {ser.portstr}")
+
+try:
+    while True:
+        if ser.in_waiting:  # jeśli są dane w buforze
+            line = ser.readline().decode(errors='ignore').strip()
+            if line:
+                print(f"Odebrano: {line}")
+except KeyboardInterrupt:
+    print("\nZamykam...")
+    ser.close()
