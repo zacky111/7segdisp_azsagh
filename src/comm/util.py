7afec3752de7cf1ddeaa2202ab93dc7d4c65db63
Jakub Ciura <jakub.ciura@gmail.com>
2025-08-19 11:27:49 +0200

    v3

--- src/comm/util.py
+++ src/comm/util.py
@@ -3,9 +3,9 @@ import serial
 from src.comm.config import PORT, BAUD, parity, stopbits, bytesize, timeout
 
 def ser_init():
-    ser = serial.Serial(PORT, BAUD, parity=parity,
-                    stopbits=stopbits,
-                    bytesize=bytesize,
-                    timeout=timeout)
+    ser = serial.Serial(PORT, BAUD, parity,
+                    stopbits,
+                    bytesize,
+                    timeout)
     print(f"Otwarty port: {ser.portstr}")
     return ser
