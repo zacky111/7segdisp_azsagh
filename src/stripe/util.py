bee1bdae291fa3c30d45af9094c2e9f59abbe99f
Jakub Ciura <jakub.ciura@gmail.com>
2025-12-04 17:35:59 +0100

    back to previous version

--- src/stripe/util.py
+++ src/stripe/util.py
@@ -1,5 +1,5 @@
 import board
-from neopixel import NeoPixel, neopixel
+from neopixel import NeoPixel
 
 
 import src.stripe.config as sc
@@ -57,8 +57,8 @@ def strip_init():
     pin2 = _board_pin_from_bcm(sc.LED_PIN_2)
     # NeoPixel brightness przyjmuje 0.0-1.0
     brightness = max(0.0, min(1.0, sc.LED_BRIGHTNESS / 255.0))
-    strip1 = NeoPixel(pin1, sc.LED_COUNT, brightness=brightness, auto_write=False, pixel_order=neopixel.GRB)
-    strip2 = NeoPixel(pin2, sc.LED_COUNT, brightness=brightness, auto_write=False, pixel_order=neopixel.GRB)
+    strip1 = NeoPixel(pin1, sc.LED_COUNT, brightness=brightness, auto_write=False, pixel_order=NeoPixel.GRB)
+    strip2 = NeoPixel(pin2, sc.LED_COUNT, brightness=brightness, auto_write=False, pixel_order=NeoPixel.GRB)
     # wyzeruj od razu
     clear_strip(strip1)
     clear_strip(strip2)
