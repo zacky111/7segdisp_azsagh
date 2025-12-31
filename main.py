587a9bc5b67e59723e0d56e9d66d72eab395c888
Jakub Ciura <jakub.ciura@gmail.com>
2025-12-31 10:51:15 +0100

    lack of b in front of string

--- main.py
+++ main.py
@@ -65,8 +65,8 @@ def comm_func():
         if ser.in_waiting > 0:
             raw = ser.read(ser.in_waiting)
             part = raw.decode('latin-1', errors='replace')
-            
-            if part != '\x10\x10\x10\x10\x10\x10\x10\x10\x10':
+
+            if part != b'\x10\x10\x10\x10\x10\x10\x10\x10\x10':
                 buffer += part
             
 
